73cd87f6aca533ebf81f29c1a0f57207358214fc
Minori <memorydewey@vip.qq.com>
2021-04-30 04:02:32 +0800

    feat: add artifacts image

---
+++ src/assets/artifacts/data/blizzardStrayer/index.js
@@ -0,0 +1,33 @@
+import cup from "./cup.png";
+import feather from "./feather.png";
+import flower from "./flower.png";
+import head from "./head.png";
+import sand from "./sand.png";
+
+export default {
+    flower: {
+        chs: "历经风雪的思念",
+        url: flower,
+    },
+    feather: {
+        chs: "摧冰而行的执望",
+        url: feather,
+    },
+    sand: {
+        chs: "冰雪故园的终期",
+        url: sand,
+    },
+    cup: {
+        chs: "遍结寒霜的傲骨",
+        url: cup,
+    },
+    head: {
+        chs: "破冰踏雪的回音",
+        url: head,
+    },
+    chs: "征服寒冬的勇士",
+    eng: "blizzardStrayer",
+    effect: "冰风迷途的勇士",
+    minStar: 4,
+    maxStar: 5,
+}
