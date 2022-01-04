af3c7117e1634606fae37455a1f00f35c2398c85
weidu <wei.du@shopee.com>
2022-01-04 18:45:26 +0800

    feat: 转义抽卡分析的字符

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,11 +1,17 @@
-import qs from 'querystring'
+import { parse } from 'querystring'
+
+export function decodeHTML(str: string) {
+  return str.replace(/&lt;|&gt;|&amp;/g, matches => {
+    return { '&lt;': '<', '&gt;': '>', '&amp;': '&' }[matches]
+  })
+}
 
 export function parseUrl<T>(url: string) {
   const splitArr = url.split('?')
   if (splitArr.length != 2) {
     return null
   }
-  return JSON.parse(JSON.stringify(qs.parse(splitArr[1]))) as unknown as T
+  return JSON.parse(JSON.stringify(parse(decodeHTML(splitArr[1])))) as unknown as T
 }
 
 export function validateParamKey(params: object, keys: string[]) {
